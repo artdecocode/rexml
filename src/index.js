923e2cc6969d9ea07ad807b7b5b598ed02ad5ca0
Anton <anton@adc.sh>
2018-07-18 21:55:21 +0100

    implement

--- src/index.js
+++ src/index.js
@@ -1,21 +1,73 @@
-import { debuglog } from 'util'
+const execRes = (re, s) => {
+  const res = re.exec(s)
+  if (!res) return res
+  const [, ...args] = res
+  return args
+}
 
-const LOG = debuglog('rexml')
+const extractProps = (s) => {
+  const propsRe = /(\w+)="(.*?)"/g
+  let t
+  const r = []
+  while ((t = propsRe.exec(s)) !== null) {
+    const [, key, value] = t
+    r.push({
+      key,
+      value: getPropValue(value),
+    })
+  }
+  const tt = s.replace(propsRe, '').trim().split(' ')
+    .filter(a => a)
+    .reduce((a, k) => ({ ...a, [k]: true }), {})
+  return r.reduce((acc, { key, value }) => ({
+    ...acc,
+    [key]: value,
+  }), tt)
+}
 
-/**
- * Simple XML parsing with a regular expression.
- * @param {Config} config Configuration object.
- * @param {string} config.type The type.
- */
-export default async function rexml(config = {}) {
-  const {
-    type,
-  } = config
-  LOG('rexml called with %s', type)
-  return type
+const getPropValue = (val) => {
+  if (val == 'true') return true
+  if (val == 'false') return false
+  if (/^\d+$/.test(val)) {
+    return parseInt(val, 10)
+  }
+  return val
 }
 
 /**
- * @typedef {Object} Config
- * @property {string} type The type.
+ * Extract member elements from an XML string.
+ * @param {string} tag Which tag to extract, e.g., `div`.
+ * @param {string} string The XML string.
+ * @example
+ *
+ * const xml = `
+<html>
+  <div id="test" class="t" contenteditable>
+    Hello World
+  </div>
+</html>
+`
+ * const [{ content, props }] = extractTag('div', xml)
+ * // content: Hello World
+ * // props: { id: 'test', class: 't', contenteditable: true }
  */
+export const extractTags = (tag, string) => {
+  const re = new RegExp(`<${tag}( .[^>]+)?(?: /)?>(?:([\\s\\S]+?)</${tag}>)?`, 'g')
+  const r = []
+
+  let t
+  while ((t = execRes(re, string))) {
+    if (!t.length) continue
+    const [_p = '', _c = ''] = t
+    const p = _p.replace(/\/$/, '').trim()
+    const props = extractProps(p)
+    const item = {
+      props,
+      content: _c,
+    }
+    r.push(item)
+  }
+  return r
+}
+
+export default extractTags

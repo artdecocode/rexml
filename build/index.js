0a9db362c84baf2c3fab86ea3e5172a86e138b6e
Anton <anton@adc.sh>
2018-07-19 17:21:24 +0100

    build

--- build/index.js
+++ build/index.js
@@ -5,46 +5,15 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = exports.extractTags = void 0;
 
+var _lib = require("./lib");
+
+var _res = require("./lib/res");
+
 const execRes = (re, s) => {
   const res = re.exec(s);
   if (!res) return res;
   const [, ...args] = res;
   return args;
-};
-
-const extractProps = s => {
-  const propsRe = /(\w+)="(.*?)"/g;
-  let t;
-  const r = [];
-
-  while ((t = propsRe.exec(s)) !== null) {
-    const [, key, value] = t;
-    r.push({
-      key,
-      value: getPropValue(value)
-    });
-  }
-
-  const tt = s.replace(propsRe, '').trim().split(/\s+/).filter(a => a).reduce((a, k) => ({ ...a,
-    [k]: true
-  }), {});
-  return r.reduce((acc, {
-    key,
-    value
-  }) => ({ ...acc,
-    [key]: value
-  }), tt);
-};
-
-const getPropValue = val => {
-  if (val == 'true') return true;
-  if (val == 'false') return false;
-
-  if (/^\d+$/.test(val)) {
-    return parseInt(val, 10);
-  }
-
-  return val;
 };
 /**
  * Extract member elements from an XML string. Numbers and booleans will be parsed into their JS types.
@@ -66,9 +35,9 @@ const getPropValue = val => {
 
 
 const extractTags = (tag, string) => {
-  const end1 = new RegExp(' />');
+  const end1 = /\s*\/>/;
   const end2 = new RegExp(`>([\\s\\S]+?)?</${tag}>`);
-  const re = new RegExp(`<${tag}(\\s+[^>]+?)?(?:${end1.source}|${end2.source})`, 'g');
+  const re = new RegExp(`<${tag}${_res.attributesRe.source}?(?:${end1.source}|${end2.source})`, 'gu');
   const r = [];
   let t;
 
@@ -78,7 +47,7 @@ const extractTags = (tag, string) => {
 
     const p = _p.replace(/\/$/, '').trim();
 
-    const props = extractProps(p);
+    const props = (0, _lib.extractProps)(p);
     const item = {
       props,
       content: _c
---
+++ build/lib/index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.extractProps = void 0;
+
+var _res = require("./res");
+
+const getPropValue = val => {
+  if (val == 'true') return true;
+  if (val == 'false') return false;
+
+  if (/^\d+$/.test(val)) {
+    return parseInt(val, 10);
+  }
+
+  return val;
+};
+/**
+ * Parse the arguments part of an XML element
+ * @param {string} s String to extract attributes from
+ */
+
+
+const extractProps = s => {
+  const o = {};
+  const re = new RegExp(_res.attributeRe.source, 'gu');
+  s.replace(re, (match, name, value) => {
+    const v = value === undefined ? true : value;
+    const val = getPropValue(v);
+    o[name] = val;
+  });
+  return o;
+};
+
+exports.extractProps = extractProps;
+//# sourceMappingURL=index.js.map
